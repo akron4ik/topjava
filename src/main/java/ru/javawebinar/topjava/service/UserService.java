0d79d5862bdc718e8a194a1540199fe1332b269f
Akron4ik <aronov.ak@gmail.com>
2019-08-15 23:12:50 +0300

    10_11, 10_12, 10_13, 10_14, 10_15, 10_16

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -15,10 +16,10 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
-import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
 
+import static ru.javawebinar.topjava.util.UserUtil.prepareToSave;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
@@ -26,59 +27,62 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserService implements UserDetailsService {
 
-        private final UserRepository repository;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-        @Autowired
-        public UserService(UserRepository repository) {
-            this.repository = repository;
-        }
+    @Autowired
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-        @CacheEvict(value = "users", allEntries = true)
-        public User create(User user) {
-            Assert.notNull(user, "user must not be null");
-            return repository.save(user);
-        }
 
-        @CacheEvict(value = "users", allEntries = true)
-        public void delete(int id) {
-            checkNotFoundWithId(repository.delete(id), id);
-        }
+    @CacheEvict(value = "users", allEntries = true)
+    public User create(User user) {
+        Assert.notNull(user, "user must not be null");
+        return repository.save(prepareToSave(user, passwordEncoder));
+    }
 
-        public User get(int id) {
-            return checkNotFoundWithId(repository.get(id), id);
-        }
+    @CacheEvict(value = "users", allEntries = true)
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
 
-        public User getByEmail(String email) {
-            Assert.notNull(email, "email must not be null");
-            return checkNotFound(repository.getByEmail(email), "email=" + email);
-        }
+    public User get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
 
-        @Cacheable("users")
-        public List<User> getAll() {
-            return repository.getAll();
-        }
+    public User getByEmail(String email) {
+        Assert.notNull(email, "email must not be null");
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
 
-        @CacheEvict(value = "users", allEntries = true)
-        public void update(User user) {
-            Assert.notNull(user, "user must not be null");
+    @Cacheable("users")
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    public void update(User user) {
+        Assert.notNull(user, "user must not be null");
 //      checkNotFoundWithId : check works only for JDBC, disabled
-            repository.save(user);
-        }
+        repository.save(prepareToSave(user, passwordEncoder));
+    }
 
-        @CacheEvict(value = "users", allEntries = true)
-        @Transactional
-        public void update(UserTo userTo) {
-            User user = get(userTo.id());
-            repository.save(UserUtil.updateFromTo(user, userTo));
-        }
+    @CacheEvict(value = "users", allEntries = true)
+    @Transactional
+    public void update(UserTo userTo) {
+        User user = get(userTo.id());
+        repository.save(prepareToSave(UserUtil.updateFromTo(user, userTo), passwordEncoder));
+    }
 
-        @CacheEvict(value = "users", allEntries = true)
-        @Transactional
-        public void enable(int id, boolean enabled) {
-            User user = get(id);
-            user.setEnabled(enabled);
-            repository.save(user);  // !! need only for JDBC implementation
-        }
+    @CacheEvict(value = "users", allEntries = true)
+    @Transactional
+    public void enable(int id, boolean enabled) {
+        User user = get(id);
+        user.setEnabled(enabled);
+        repository.save(user);  // !! need only for JDBC implementation
+    }
 
     @Override
     public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -89,7 +93,7 @@ public class UserService implements UserDetailsService {
         return new AuthorizedUser(user);
     }
 
-        public User getWithMeals(int id) {
-            return checkNotFoundWithId(repository.getWithMeals(id), id);
-        }
+    public User getWithMeals(int id) {
+        return checkNotFoundWithId(repository.getWithMeals(id), id);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
@@ -35,6 +35,14 @@ class AdminRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    void getNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
+
+    @Test
     void getByEmail() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(REST_URL + "by?email=" + ADMIN.getEmail())
                 .with(userHttpBasic(ADMIN)))
@@ -50,6 +58,14 @@ class AdminRestControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(status().isNoContent());
         assertMatch(userService.getAll(), ADMIN);
+    }
+
+    @Test
+    void deleteNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
     }
 
     @Test
@@ -85,7 +101,7 @@ class AdminRestControllerTest extends AbstractControllerTest {
         ResultActions action = mockMvc.perform(MockMvcRequestBuilders.post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .with(userHttpBasic(ADMIN))
-                .content(JsonUtil.writeValue(expected)))
+                .content(jsonWithPassword(expected, "newPass")))
                 .andExpect(status().isCreated());
 
         User returned = readFromJson(action, User.class);
